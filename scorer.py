b6d24778c4030ad7872261fc2ce43a9b6153fd4c
ahsanMah <mahmood.ahsan@outlook.com>
2024-06-17 17:04:47 -0400

    Added gmm functionality

--- scorer.py
+++ scorer.py
@@ -1,13 +1,18 @@
 import os
 import pickle
+from pickle import dump, load
 
 import numpy as np
 import PIL.Image
 import torch
+from sklearn.mixture import GaussianMixture
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
 
 import dnnlib
 from training.dataset import ImageFolderDataset
-from tqdm import tqdm
+
 
 class EDMScorer(torch.nn.Module):
     def __init__(
@@ -42,9 +47,7 @@ class EDMScorer(torch.nn.Module):
         ) ** rho
         print("Using steps:", t_steps)
 
-        self.register_buffer(
-            "sigma_steps", t_steps.to(torch.float64)
-        )
+        self.register_buffer("sigma_steps", t_steps.to(torch.float64))
 
     @torch.inference_mode()
     def forward(
@@ -67,8 +70,8 @@ class EDMScorer(torch.nn.Module):
 
         return batch_scores
 
-def build_model(netpath = f"edm2-img64-s-1073741-0.075.pkl", device='cpu'):
 
+def build_model(netpath=f"edm2-img64-s-1073741-0.075.pkl", device="cpu"):
     model_root = "https://nvlabs-fi-cdn.nvidia.com/edm2/posthoc-reconstructions"
     netpath = f"{model_root}/{netpath}"
     with dnnlib.util.open_url(netpath, verbose=1) as f:
@@ -78,7 +81,34 @@ def build_model(netpath = f"edm2-img64-s-1073741-0.075.pkl", device='cpu'):
     return model
 
 
-def test_runner(device='cpu'):
+def train_gmm(score_path, outdir="out/msma/"):
+    X = torch.load(score_path)
+
+    gm = GaussianMixture(n_components=5, random_state=42)
+    clf = Pipeline([("scaler", StandardScaler()), ("GMM", gm)])
+    clf.fit(X)
+    inlier_nll = -clf.score_samples(X)
+
+    with open(f"{outdir}/refscores.npz", "wb") as f:
+        np.savez_compressed(f, inlier_nll)
+
+    with open(f"{outdir}/gmm.pkl", "wb") as f:
+        dump(clf, f, protocol=5)
+
+
+def compute_gmm_likelihood(x_score, gmmdir):
+    with open(f"{gmmdir}/gmm.pkl", "rb") as f:
+        clf = load(f)
+        nll = -clf.score_samples(x_score)
+
+    with np.load(f"{gmmdir}/refscores.npz", "wb") as f:
+        ref_nll = f["arr_0"]
+        percentile = (ref_nll < nll).mean()
+
+    return nll, percentile
+
+
+def test_runner(device="cpu"):
     f = "goldfish.JPEG"
     image = (PIL.Image.open(f)).resize((64, 64), PIL.Image.Resampling.LANCZOS)
     image = np.array(image)
@@ -88,30 +118,37 @@ def test_runner(device='cpu'):
     scores = model(x)
     return scores
 
-def runner(dataset_path, device='cpu'):
+
+def runner(dataset_path, device="cpu"):
     dsobj = ImageFolderDataset(path=dataset_path, resolution=64)
     refimg, reflabel = dsobj[0]
     print(refimg.shape, refimg.dtype, reflabel)
-    dsloader = torch.utils.data.DataLoader(dsobj, batch_size=48, num_workers=4, prefetch_factor=2)
-    
-    model = build_model(device=device) 
+    dsloader = torch.utils.data.DataLoader(
+        dsobj, batch_size=48, num_workers=4, prefetch_factor=2
+    )
+
+    model = build_model(device=device)
     score_norms = []
 
-    for x,_ in tqdm(dsloader):
+    for x, _ in tqdm(dsloader):
         s = model(x.to(device))
-        s = s.square().sum(dim=(2,3,4)) ** 0.5
+        s = s.square().sum(dim=(2, 3, 4)) ** 0.5
         score_norms.append(s.cpu())
 
     score_norms = torch.cat(score_norms, dim=0)
-    
-    os.makedirs('out/msma', exist_ok=True)
-    with open('out/msma/imagenette64_score_norms.pt', 'wb') as f:
+
+    os.makedirs("out/msma", exist_ok=True)
+    with open("out/msma/imagenette64_score_norms.pt", "wb") as f:
         torch.save(score_norms, f)
 
-    print(f'Computed score norms for {score_norms.shape[0]} samples')
+    print(f"Computed score norms for {score_norms.shape[0]} samples")
+
 
 if __name__ == "__main__":
-    #s = test_runner()
-    #print(s)
-    runner("/GROND_STOR/amahmood/datasets/img64/", device='cuda')
-
+    # runner("/GROND_STOR/amahmood/datasets/img64/", device="cuda")
+    train_gmm("out/msma/imagenette64_score_norms.pt")
+    s = test_runner(device="cuda")
+    s = s.square().sum(dim=(2, 3, 4)) ** 0.5
+    s = s.to("cpu").numpy()
+    nll, pct = compute_gmm_likelihood(s, gmmdir="out/msma/")
+    print(f"Anomaly score for image: {nll[0]:.3f} @ {pct*100:.2f} percentile")
